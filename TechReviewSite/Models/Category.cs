5bac594b2658027796ff52fd060a13a9194e6955
Joseph Miklus <Joseph Miklus>
2016-09-06 17:31:51 -0400

    First commit

---
+++ TechReviewSite/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TechReviewSite.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Category")]
+        public string Name { get; set; }
+    }
+}
---
+++ TechReviewSite/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TechReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }                 //Key ID number for each review
+        public string Title { get; set; }           //Title of the review
+        public string Text { get; set; }            //Body text of the review
+        public int Rating { get; set; }             //Rating out of 5 stars
+        public DateTime PublishDate { get; set; }   //Date the review was written
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
